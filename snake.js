dc0ba79166b177b3f051f5e7dd5d5912ee63b817
Rodrigo Eleuterio <roeleuss@gmail.com>
2024-06-08 23:58:56 -0300

    fix: snake moving outside area

--- snake.js
+++ snake.js
@@ -50,12 +50,6 @@ function update(event) {
 
 // Main game loop
 function startGame() {
-    // If the snake goes off the edge of the grid, make it appear on the opposite side
-    if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
-    if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
-    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
-    if (snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
-
     // Check if the snake has collided with itself
     for (i = 1; i < snake.length; i++) {
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
@@ -77,6 +71,12 @@ function startGame() {
     if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
+
+    // If the snake goes off the edge of the grid, make it appear on the opposite side
+    if (snakeX > 15 * box) snakeX = 0;
+    if (snakeX < 0) snakeX = 15 * box;
+    if (snakeY > 15 * box) snakeY = 0;
+    if (snakeY < 0) snakeY = 15 * box;
 
     // If the snake has eaten the food, generate a new food position
     // Otherwise, remove the last segment of the snake to maintain the same length
